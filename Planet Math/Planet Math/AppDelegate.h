91e0d1d21a8bc740ab0c0bde6468eebb49971786
Samuel Arseneault <samarseneault@yahoo.com>
2015-02-01 20:12:25 -0800

    Created a Planet Math IOS Application in Xcode

---
+++ Planet Math/Planet Math/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Planet Math
+//
+//  Created by Samuel Arseneault on 2/1/15.
+//  Copyright (c) 2015 Samuel Arseneault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Planet Math/Planet Math/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Planet Math
+//
+//  Created by Samuel Arseneault on 2/1/15.
+//  Copyright (c) 2015 Samuel Arseneault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
