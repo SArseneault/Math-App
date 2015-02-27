4277288ef70dd93654f321cab87ea2f4f09e8858
Johnluu907 <johnluu907@gmail.com>
2015-02-27 09:54:27 -0500

    Tile is now able to move, a new scene was made to view movement

---
+++ MathApplication/PirateMathApplication/TileView.h
@@ -0,0 +1,13 @@
+//
+//  TileView.h
+//  PirateMathApplication
+//
+//  Created by John Luu on 2/26/15.
+//  Copyright (c) 2015 Samuel Arseneault. All rights reserved.
+//
+
+#ifndef PirateMathApplication_TileView_h
+#define PirateMathApplication_TileView_h
+
+
+#endif
