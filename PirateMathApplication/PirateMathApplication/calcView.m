4ac539d9217868052fa2dbf55fc4a604d6e73f69
Kelly Markaity <kmark21@gmail.com>
2015-03-01 23:03:23 -0500

    Reset practice after completing X problems bug fixed and corrected

--- PirateMathApplication/PirateMathApplication/calcView.m
+++ PirateMathApplication/PirateMathApplication/calcView.m
@@ -31,13 +31,16 @@
 //method to setup level
 -(void) setUpLevel
 {
+    //set seconds to 0
+    seconds =0;
+    
     //set question asked to 0
     questionCount = 0;
     
     //set questions correct to 0
     totalQuestionsCorrect = 0;
     
-    //create timer object, call to method increasTime to increase and display current time spent on level
+    //create timer object, call to method increaseTime to increase and display current time spent on level
     timer = [NSTimer scheduledTimerWithTimeInterval:1.0F target:self selector:@selector(increaseTime) userInfo: nil repeats:YES];
     
     //call to generate random number method to start game
@@ -47,26 +50,9 @@
 
 -(void) generateNumber
 {
-    
-
-    //CHECK IF QUESTION COUNT IS AT 15, P
-    if(questionCount ==5)
-    {
-        //stop timer
-        [timer invalidate];
-        
-        //alert to show that the practice section is over
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Practice Over" message:[NSString stringWithFormat:@"You got %ld out of 15 questions correct\n Total Time: %ld seconds", totalQuestionsCorrect,seconds] delegate:self cancelButtonTitle:@"PlayAgain?" otherButtonTitles:nil];
-        
-        [alert show];
-        
-    }
-    
     //increment question counter
     questionCount++;
 
-    
-    
     //generates random number between 0 and 10
     valueOne =arc4random()%10;
     valueTwo=arc4random()%10;
@@ -75,7 +61,6 @@
     firstNumber.text =[NSString stringWithFormat:@"%ld",valueOne];
     secondNumber.text =[NSString stringWithFormat:@"%ld",valueTwo];
     
-    
     //clear user input textbox
     userInput.text = @"";
 
@@ -83,10 +68,10 @@
 
 -(IBAction)submitAnswer
 {
-    //find and store answer
+    //calulate answer
     correctAnswer =valueOne +valueTwo;
     
-    //take user input
+    //get user input
     userAnswer =([userInput.text integerValue]);
     
     
@@ -98,6 +83,7 @@
         
         //alert to show that the user was correct
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Correct!" message:[NSString stringWithFormat:@"%ld + %ld = %ld",valueOne,valueTwo,correctAnswer] delegate:self cancelButtonTitle:@"Continue" otherButtonTitles:nil];
+        [alert setTag:2];
         [alert show];
         
     }
@@ -105,11 +91,47 @@
     {
         //alert to show the users input was wrong
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Incorrect!" message:[NSString stringWithFormat:@"%ld + %ld = %ld",valueOne,valueTwo,correctAnswer] delegate:self cancelButtonTitle:@"Continue" otherButtonTitles:nil];
+        [alert setTag:3];
         [alert show];
     }
     
-    //call generate Number method to create new question
-    [self generateNumber];
+    //call method to check for end of practice section
+    [self isEndCheck];
+    
+
+}
+
+-(void) isEndCheck
+{
+    //CHECK IF QUESTION COUNT IS AT 5(FOR TESTING ONLY)
+    if(questionCount == 5)
+    {
+        //call method to display end results
+        [self endResult];
+        
+    }
+    else
+    {
+        //call generate Number method to create new question
+        [self generateNumber];
+    }
+    
+    
+}
+
+//end result method that displays results, right/wrong and time taken to complete
+-(void) endResult
+{
+    //stop timer
+    [timer invalidate];
+    
+    //alert to show that the practice section is over
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Practice Over" message:[NSString stringWithFormat:@"You got %ld out of 15 questions correct\n Total Time: %ld seconds", totalQuestionsCorrect,seconds] delegate:self cancelButtonTitle:@"PlayAgain?" otherButtonTitles:nil];
+    
+    //set alert tag to endTag
+    [alert setTag:1];
+    [alert show];
+    
 }
 
 //timer method to increase time, fired every second
@@ -120,10 +142,18 @@
     
     //update timmer label
     labelForTimer.text = [NSString stringWithFormat:@"Time: %ld", seconds];
+}
+
+//method for when UIalert button is pressed, helps diferentiate between which one is pressed
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    if(alertView.tag ==1)   //end alert view was pressed
+    {
+        //calls to reset level
+        [self setUpLevel];
+        
+    }
     
     
 }
-
-
-
 @end
