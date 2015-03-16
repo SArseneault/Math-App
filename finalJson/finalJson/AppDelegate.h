dc4dcb618564a927d9a37954d32619ed939b916e
Kelly Markaity <kmark21@gmail.com>
2015-03-16 16:27:57 -0400

    Final Json Project to show basic son formatting

---
+++ finalJson/finalJson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  finalJson
+//
+//  Created by Kelly Markaity on 3/16/15.
+//  Copyright (c) 2015 Kelly Markaity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
