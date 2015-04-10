083ba1ae1c99cf7428af6142ae1f33ddbccc1095
Kelly Markaity <kmark21@gmail.com>
2015-04-10 02:22:47 -0400

    Changed the background color, font color and font size of tiles. Still need to changed the font style

--- GraylingMathApp/GraylingMathApp/TileUiView.m
+++ GraylingMathApp/GraylingMathApp/TileUiView.m
@@ -15,11 +15,12 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.label               = [[UILabel alloc] init];
-        self.label.font          = [UIFont boldSystemFontOfSize:48];
+        self.label.font          = [UIFont boldSystemFontOfSize:86];
         self.label.textAlignment = NSTextAlignmentCenter;
+        self.label.textColor =[UIColor colorWithRed:255/255.0f green:0/255.0f blue:4/255.0f alpha:1.0f];
         [self addSubview:self.label];
         
-        self.backgroundColor    = [UIColor whiteColor];
+        self.backgroundColor    = [UIColor colorWithRed:201/255.0f green:201/255.0f blue:201/255.0f alpha:1.0f];
         self.layer.cornerRadius = 10.0f;
     }
     return self;
