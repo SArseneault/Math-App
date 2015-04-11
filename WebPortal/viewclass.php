996b62a8443729ae3f6a5f0adec1ccdf96acfb55
Johnluu907 <johnluu907@gmail.com>
2015-04-11 16:05:47 -0400

    Added the d3.js bar graph to the viewclass.php file with dummy data.

--- WebPortal/viewclass.php
+++ WebPortal/viewclass.php
@@ -552,14 +552,111 @@ if($user->classExist()){  ?>
 
       </div>
       <div class="modal-body">
-       
+        //d3.js bar graph with dummy data
+       <style>
 
-       <div class="modal-body">
-        <img src = "barGraph2.php">
+    body {
+      font: 10px sans-serif;
+    }
+
+    .axis path,
+    .axis line {
+      fill: none;
+      stroke: #000;
+      shape-rendering: crispEdges;
+    }
+
+    .bar {
+      fill: steelblue;
+    }
+
+    .x.axis path {
+      display: none;
+    }
+
+    </style>
+    <body>
+        <div id="bar"></div>
+    <script src="http://d3js.org/d3.v3.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script>
+    var $window = $(window);
+    var wWidth  = $window.width();
+    var wHeight = $window.height();
+
+    var margin = {top: 10, right: 0, bottom: 30, left: 40},
+        width = 300 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+    //var formatPercent = d3.format("1");
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+    //var data = [[1,1],[2,3],[3,2],[4,5],[5,4]];
+    var svg = d3.select("#bar").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+        //dummy data
+    var data = [
+        {level:"Level 1",frequency:"4"},
+        {level:"Level 2",frequency:"5"},
+        {level:"Level 3",frequency:"2"},
+        {level:"Level 4",frequency:"1"},
+        {level:"Level 5",frequency:"6"}
+    ];
+
+      x.domain(data.map(function(d) { return d.level; }));
+      y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform","rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Progress");
+
+      svg.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.level); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.frequency); })
+          .attr("height", function(d) { return height - y(d.frequency); });
+
+  
+    function type(d) {
+      d.frequency = +d.frequency;
+      return d;
+    }
+
+    </script>
+
+
+
       </div>
-
-
-
       </div>
         <div class="modal-footer">
         <button type="button" id="refreshpage" class="btn btn-default" data-dismiss="modal">Close</button>
